aa30aa1409bfc21e3e30192cfb3738cd93c1153f
Matthew Wolf <thisismattwolf@gmail.com>
2019-05-19 18:19:47 +0200

    Split the plot_loans and plot_portfolios functions into 2 each: one that plots net income vs impact ratings, and one that plots net income vs default risk, for both Loans and Portfolios. Need to decide on what basis to aggregate risk, impact, and net income for Portfolios - weighted average? Total? tbd

--- efficient_impact_frontier_construction.py
+++ efficient_impact_frontier_construction.py
@@ -18,7 +18,7 @@ def data_prepare():
     This function loads and prepares the loans data to be used in the analysis.
     It creates a Pandas df from the existing CSV of loans data, and uses each
     row of data to create a Loan class instance (see the structure_and_methods.py
-    file) of that loan, and stores these instances in another column in the df.    
+    file) of that loan, and stores these instances in another column in the df.
     ====================================="""
     
     import pandas as pd
@@ -84,36 +84,58 @@ def data_prepare():
     # return the prepared df
     return data
 
-def plot_loans(loans):
+def plot_loans_impact(loans):
     """========================================================================
-    This function uses the seaborn lib to plot a set of Loan() class instances
-    along two axes - net income, and impact rating.
-    The input 'loans' should be a Pandas dataframe with Loan() class instances 
-    for the loans to be plotted in a column named 'Loan Object'.
+    Plots loans' Root Capital Impact Score (X) vs. Net Income (Y)
+    
+    INPUT:   Pandas one-column DF of Loan object instances
+    OUTPUT:  Seaborn Chart
     ========================================================================"""
     import pandas, seaborn
     df = pandas.DataFrame()
     for i in range(len(loans)):
         df.at[i, 'Loan Object'] = loans.at[i, 'Loan Object']
-        df.at[i, 'Expected Impact Rating'] = loans.at[i, 'Loan Object'].get_root_capital_impact_rating()
-        df.at[i, 'Net Income'] = loans.at[i, 'Loan Object'].get_exp_net_income()
+        df.at[i, 'Expected Impact Rating (0-10)'] = loans.at[i, 'Loan Object'].get_root_capital_impact_rating()
+        df.at[i, 'Net Income ($)'] = loans.at[i, 'Loan Object'].get_exp_net_income()
         df.at[i, 'Impact Group'] = loans.at[i, 'Loan Object'].get_root_capital_impact_group()
-    print(df.head())
-    chart = seaborn.lmplot(x='Expected Impact Rating',
-           y='Net Income',
+    chart = seaborn.lmplot(x='Expected Impact Rating (0-10)',
+           y='Net Income ($)',
            data=df,
            hue='Impact Group',
            fit_reg=False)
-    #chart.set(ylim=(minIncome,maxIncome))
-    #chart.set(xlim=(minImpact,maxImpact))
-
-def randomPortfolios(loans, r, n):
+    
+def plot_loans_risk(loans):
     """========================================================================
-    This function takes a list of Loan() objects in a Pandas dataframe, as well
-    as two number: the desired number of random portfolios (r), and the number of 
-    loans per portfolio (n). It then builds r Portfolio() instances of n Loan
-    instances, and returns them in a Pandas dataframe with the column header, 
-    "Portfolio Object".
+    Plots loans' Risk of Default (X) vs. Net Income (Y)
+    
+    INPUT:   Pandas one-column DF of Loan object instances
+    OUTPUT:  Seaborn Chart
+    ========================================================================"""
+    import pandas, seaborn
+    df = pandas.DataFrame()
+    for i in range(len(loans)):
+        df.at[i, 'Loan Object'] = loans.at[i, 'Loan Object']
+        df.at[i, 'Default Risk (%)'] = loans.at[i, 'Loan Object'].get_default_prob()
+        df.at[i, 'Net Income ($)'] = loans.at[i, 'Loan Object'].get_exp_net_income()
+        df.at[i, 'Impact Group'] = loans.at[i, 'Loan Object'].get_root_capital_impact_group()
+    chart = seaborn.lmplot(x='Default Risk (%)',
+           y='Net Income ($)',
+           data=df,
+           hue='Impact Group',
+           fit_reg=False)
+
+def random_portfolios_by_n(loans, r, n):
+    """========================================================================
+    Randomly selects n Loan object instances from a list to construct a random 
+    Portfolio object instance. Repeats this r times, for a list of r random
+    Portfolio instances.
+    
+    No constraints on which Loans may be chosen.
+    
+    INPUTS:     loans       - Pandas one-column DF of Loan object instances
+                r           - number of random portfolios to be constructed
+                n           - number of Loan instances per random portfolio
+    OUTPUT:     portfolios  - Pandas one-column DF of Portfolio object instances
     ========================================================================"""
     import random
     import pandas as pd
@@ -125,13 +147,14 @@ def randomPortfolios(loans, r, n):
     portfolios = pd.DataFrame(portfolios, columns=["Portfolio Object"])
     return portfolios
 
-def plot_portfolios(portfolios):
-
+#TO-DO Allow choice between total and weighted average impact metrics and 
+    # net incomes. 
+def plot_portfolios_impact(portfolios):
     """========================================================================
-    This function uses the seaborn lib to plot a set of Portfolio() class
-    instances along two axes - net income and impact rating.
-    The input 'portoflios' should be a Pandas dataframe with Portfolio() class 
-    instances for the loans to be plotted in a column named 'Portfolio Object'.
+    Plots Portofolios' Root Capital Impact Score (X) vs. Net Income (Y)
+    
+    INPUT:   Pandas one-column DF of Portfolio object instances
+    OUTPUT:  Seaborn Chart
     ========================================================================"""
     import pandas, seaborn
     
@@ -144,7 +167,28 @@ def plot_portfolios(portfolios):
         
     chart = seaborn.lmplot(x='Impact Rating', y='Required Subsidy', data=df,\
                            hue='Impact Group', fit_reg=False)
-      
+
+#TO DO: Create weighted avg default risk getter for Portfolio class. Allow 
+    #choice between total and weighted avg net income metrics
+def plot_portfolios_risk(portfolios):
+    """========================================================================
+    Plots Portofolios' Risk of Default (X) vs. Net Income (Y)
+    
+    INPUT:   Pandas one-column DF of Portfolio object instances
+    OUTPUT:  Seaborn Chart
+    ========================================================================"""
+    import pandas, seaborn
+    
+    df = pandas.DataFrame()
+    for i in range(len(portfolios)):
+        df.at[i, 'Portfolio Object'] = portfolios.at[i, 'Portfolio Object']
+        df.at[i, 'Required Subsidy'] = portfolios.at[i, 'Portfolio Object'].get_total_net_income()
+        df.at[i, 'Impact Rating'] = portfolios.at[i, 'Portfolio Object'].get_total_impact_rating()
+        df.at[i, 'Impact Group'] = portfolios.at[i, 'Portfolio Object'].get_total_impact_group()
+        
+    chart = seaborn.lmplot(x='Impact Rating', y='Required Subsidy', data=df,\
+                           hue='Impact Group', fit_reg=False)
+       
 #TODO
         # 1. Allow Loan() class to take user-specified impact heuristic
         # 2. Allow Portfolio class to take user-specified impact heuristic
